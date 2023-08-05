90df8a11256d67ebc22c4dfe91838ec512ceb95f
Emmanuel Ajala <emzzypra@gmail.com>
2023-08-05 22:05:33 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#49a4ac',
+        textprimary: '#81b6c4',
+        textsecondary: '#38b4be',
+      }
+    },
+  },
+  plugins: [],
+}
+
