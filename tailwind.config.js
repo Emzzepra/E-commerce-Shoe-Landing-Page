0a1e81468939d44ba542db9cf4a50696c14752bc
Oluwademilade Ala <demiladeala@gmail.com>
2023-08-10 17:44:52 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html","./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         primary: '#49a4ac',
         textprimary: '#81b6c4',
         textsecondary: '#38b4be',
-      }
+      },
     },
   },
   plugins: [],
 }
-
-
